69a32b3354f47338ff9078f78911c92407776679
Iqbal Bakhtiar <iqbal_bakhtiar@yahoo.com>
2025-06-21 18:44:39 +0700

    Improvement Purchase Report & Billing Print

--- src/main/java/procurement/com/siriuserp/procurement/query/PurchaseReportQuery.java
+++ src/main/java/procurement/com/siriuserp/procurement/query/PurchaseReportQuery.java
@@ -4,6 +4,8 @@
  */
 package com.siriuserp.procurement.query;
 
+import java.util.List;
+
 import org.hibernate.Query;
 
 import com.siriuserp.procurement.criteria.PurchaseReportFilterCriteria;
@@ -25,10 +27,59 @@ public class PurchaseReportQuery extends AbstractStandardReportQuery
 		PurchaseReportFilterCriteria criteria = (PurchaseReportFilterCriteria) getFilterCriteria();
 
 		StringBuilder builder = new StringBuilder();
-		builder.append("SELECT new com.siriuserp.procurement.adapter.PurchaseReportAdapter(purchaseItem) ");
+		builder.append("SELECT NEW com.siriuserp.procurement.adapter.PurchaseReportAdapter(purchaseItem) ");
 		builder.append("FROM PurchaseOrderItem purchaseItem ");
 		builder.append("WHERE purchaseItem.purchaseOrder.organization.id =:org ");
 		builder.append("AND purchaseItem.purchaseItemType = 'BASE' ");
+		builder.append("AND (purchaseItem.id IN(:noApprovable) OR purchaseItem.id IN(:withApprovable)) ");
+
+		if (SiriusValidator.validateParamWithZeroPosibility(criteria.getSupplier()))
+			builder.append("AND purchaseItem.purchaseOrder.supplier.id =:supplier ");
+
+		if (SiriusValidator.validateParam(criteria.getDocumentType()))
+			builder.append("AND purchaseItem.purchaseOrder.purchaseDocumentType =:documentType ");
+
+		if (SiriusValidator.validateDate(criteria.getDateFrom()))
+		{
+			if (SiriusValidator.validateDate(criteria.getDateTo()))
+				builder.append("AND purchaseItem.purchaseOrder.date BETWEEN :from AND :to ");
+			else
+				builder.append("AND purchaseItem.purchaseOrder.date >= :from ");
+		} else if (SiriusValidator.validateDate(criteria.getDateTo()))
+			builder.append("AND purchaseItem.purchaseOrder.date <= :to ");
+
+		builder.append("ORDER BY purchaseItem.purchaseOrder.purchaseDocumentType ASC, purchaseItem.purchaseOrder.date ASC, purchaseItem.purchaseOrder.id ASC");
+
+		Query query = getSession().createQuery(builder.toString());
+		query.setReadOnly(true);
+		query.setParameter("org", criteria.getOrganization());
+		query.setParameterList("noApprovable", getNoApprovable(criteria));
+		query.setParameterList("withApprovable", getWithApprovable(criteria));
+
+		if (SiriusValidator.validateParamWithZeroPosibility(criteria.getSupplier()))
+			query.setParameter("supplier", criteria.getSupplier());
+
+		if (SiriusValidator.validateParam(criteria.getDocumentType()))
+			query.setParameter("documentType", PurchaseDocumentType.valueOf(criteria.getDocumentType()));
+
+		if (SiriusValidator.validateDate(criteria.getDateFrom()))
+			query.setParameter("from", criteria.getDateFrom());
+
+		if (SiriusValidator.validateDate(criteria.getDateTo()))
+			query.setParameter("to", criteria.getDateTo());
+
+		return query.list();
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<Long> getNoApprovable(PurchaseReportFilterCriteria criteria)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("SELECT purchaseItem.id ");
+		builder.append("FROM PurchaseOrderItem purchaseItem ");
+		builder.append("WHERE purchaseItem.purchaseOrder.organization.id =:org ");
+		builder.append("AND purchaseItem.purchaseItemType = 'BASE' ");
+		builder.append("AND purchaseItem.purchaseOrder.approvable IS NULL ");
 
 		if (SiriusValidator.validateParamWithZeroPosibility(criteria.getSupplier()))
 			builder.append("AND purchaseItem.purchaseOrder.supplier.id =:supplier ");
@@ -62,7 +113,53 @@ public class PurchaseReportQuery extends AbstractStandardReportQuery
 
 		if (SiriusValidator.validateDate(criteria.getDateTo()))
 			query.setParameter("to", criteria.getDateTo());
-		
+
+		return query.list();
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<Long> getWithApprovable(PurchaseReportFilterCriteria criteria)
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("SELECT purchaseItem.id ");
+		builder.append("FROM PurchaseOrderItem purchaseItem ");
+		builder.append("WHERE purchaseItem.purchaseOrder.organization.id =:org ");
+		builder.append("AND purchaseItem.purchaseItemType = 'BASE' ");
+		builder.append("AND purchaseItem.purchaseOrder.approvable.approvalDecision.approvalDecisionStatus != 'REJECTED' ");
+
+		if (SiriusValidator.validateParamWithZeroPosibility(criteria.getSupplier()))
+			builder.append("AND purchaseItem.purchaseOrder.supplier.id =:supplier ");
+
+		if (SiriusValidator.validateParam(criteria.getDocumentType()))
+			builder.append("AND purchaseItem.purchaseOrder.purchaseDocumentType =:documentType ");
+
+		if (SiriusValidator.validateDate(criteria.getDateFrom()))
+		{
+			if (SiriusValidator.validateDate(criteria.getDateTo()))
+				builder.append("AND purchaseItem.purchaseOrder.date BETWEEN :from AND :to ");
+			else
+				builder.append("AND purchaseItem.purchaseOrder.date >= :from ");
+		} else if (SiriusValidator.validateDate(criteria.getDateTo()))
+			builder.append("AND purchaseItem.purchaseOrder.date <= :to ");
+
+		builder.append("ORDER BY purchaseItem.purchaseOrder.purchaseDocumentType ASC, purchaseItem.purchaseOrder.date ASC, purchaseItem.purchaseOrder.id ASC");
+
+		Query query = getSession().createQuery(builder.toString());
+		query.setReadOnly(true);
+		query.setParameter("org", criteria.getOrganization());
+
+		if (SiriusValidator.validateParamWithZeroPosibility(criteria.getSupplier()))
+			query.setParameter("supplier", criteria.getSupplier());
+
+		if (SiriusValidator.validateParam(criteria.getDocumentType()))
+			query.setParameter("documentType", PurchaseDocumentType.valueOf(criteria.getDocumentType()));
+
+		if (SiriusValidator.validateDate(criteria.getDateFrom()))
+			query.setParameter("from", criteria.getDateFrom());
+
+		if (SiriusValidator.validateDate(criteria.getDateTo()))
+			query.setParameter("to", criteria.getDateTo());
+
 		return query.list();
 	}
 }
--- src/main/java/sales/com/siriuserp/sales/dm/DeliveryOrder.java
+++ src/main/java/sales/com/siriuserp/sales/dm/DeliveryOrder.java
@@ -122,6 +122,11 @@ public class DeliveryOrder extends Model implements JSONSupport
 	@OrderBy("id")
 	private Set<DeliveryOrderItem> items = new FastSet<DeliveryOrderItem>();
 
+	public String getCode()
+	{
+		return this.code.replace("SJ", "");
+	}
+
 	public Set<SalesOrder> getSalesOrders()
 	{
 		HashSet<SalesOrder> references = new HashSet<SalesOrder>();
